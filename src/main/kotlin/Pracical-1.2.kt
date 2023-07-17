7a79fcdf1bd308f17c58f9e902584e9137a6fa5a
“smitvaishnav21” <smitvaishnav21@gnu.ac.in>
2023-07-17 21:41:57 +0530

    practical 1.2 and 1.3 done

--- src/main/kotlin/Pracical-1.2.kt
+++ src/main/kotlin/Pracical-1.2.kt
@@ -1,7 +1,16 @@
 fun main(){
-    var integer:Int=75746545;
-    println("Integer Value : $integer");
-    integer.toDouble();
-    println("double balue : $integer");
-    var integer2:Int=123;
+    var a=10
+    var b=a.toDouble()
+    var c="10"
+    var d=c.toInt()
+    var e="434.334"
+    var f=e.toDouble()
+
+    println("Integer Value:$a")
+    println("Double Value:$b")
+    println("String Value:$c")
+    println("Integer Value:$d")
+    println("String Value:$e")
+    println("Double Value:$f")
+
 }
