d0f07e42a169261c798f14b3c75e2d91c7cc7127
student <student@gnu.ac.in>
2023-07-18 08:53:19 +0530

    Updated 1.7

--- src/main/kotlin/Practical-1.7.kt
+++ src/main/kotlin/Practical-1.7.kt
@@ -1,11 +1,15 @@
 fun main(){
     var num = readLine()!!.toInt();
-    fun fact(num:Int){
-        if(num==0 || num ==1){
-            return 1;
-        }
-        else
-            return num*fact(num-1)
+    var factorial = fact(num)
+    print("factorial of $num = $factorial")
 
+}
+
+fun fact(num:Int):Long{
+    if(num==0 || num ==1){
+        return 1;
     }
+    else
+        return num*fact(num-1)
+
 }
